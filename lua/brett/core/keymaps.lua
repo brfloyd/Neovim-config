7d4667b86009b60958de458395ab2c835d423f64
brettfloyd <brettfloyd@zeroeyes.com>
2026-04-19 23:42:29 -0400

    more fixes

--- lua/brett/core/keymaps.lua
+++ lua/brett/core/keymaps.lua
@@ -11,6 +11,12 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<Esc>", function()
+  if vim.v.hlsearch == 1 then
+    return "<cmd>nohlsearch<CR>"
+  end
+  return "<Esc>"
+end, { expr = true, silent = true, desc = "Clear search highlight" })
 
 -- delete single character without copying into register
 -- keymap.set("n", "x", '"_x')
